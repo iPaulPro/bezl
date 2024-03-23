d1c8246fad92d7dbb797f13b0fb5df0a9489e5ce
Paul Burke <paul@paulburke.co>
2024-03-23 00:13:05 -0400

    feat: detects frames with frame.js

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,5 @@
+import { getFrame } from "frames.js"
+
 chrome.tabs.onUpdated.addListener(async function (_tabId, changeInfo, tab) {
     if (changeInfo.status === 'complete') {
         await checkForFrame(tab);
@@ -5,7 +7,6 @@ chrome.tabs.onUpdated.addListener(async function (_tabId, changeInfo, tab) {
 });
 //
 chrome.tabs.onActivated.addListener(function (activeInfo) {
-    console.log('Tab activated...', activeInfo);
     chrome.tabs.get(activeInfo.tabId, async     function (tab) {
         if (tab) {
             await checkForFrame(tab);
@@ -14,7 +15,6 @@ chrome.tabs.onActivated.addListener(function (activeInfo) {
 });
 
 chrome.windows.onFocusChanged.addListener(function () {
-    console.log('Window focus changed...');
     chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
         if (tabs.length > 0) {
             await checkForFrame(tabs[0]);
@@ -22,10 +22,9 @@ chrome.windows.onFocusChanged.addListener(function () {
     });
 });
 
-export const getDocument = () => document.documentElement.outerHTML;
+export const getDocument = () => document.head.outerHTML;
 
 async function checkForFrame(tab: chrome.tabs.Tab) {
-    console.log('checkOpenGraphTags: tab=', tab);
     if (!tab.id) return;
 
     try {
@@ -33,15 +32,25 @@ async function checkForFrame(tab: chrome.tabs.Tab) {
             target: {tabId: tab.id},
             func: getDocument
         });
-        const doc = injected[0]?.result ?? {};
-        console.log('doc:', doc);
+        const doc = injected[0]?.result;
+        if (!doc || !tab.url) {
+            console.error('checkForFrame: No document found');
+            return;
+        }
+        // console.log('doc:', doc);
+        const { frame } = getFrame({
+            htmlString: doc,
+            url: tab.url
+        });
+        if (frame.version) {
+            console.log('checkForFrame: Found a frame!', frame)
+        }
+
     } catch (error) {
-        console.error('Error injecting script:', error);
+        // console.error('checkForFrame: Error injecting script:', error);
     }
 }
 
 // function updateToolbarIcon(tabId?: number, hasOGTags: boolean) {
 //     // Your code to update the toolbar icon based on hasOGTags
 // }
-
-console.log('Background script loaded...');
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { crx } from '@crxjs/vite-plugin'
+import {crx, ManifestV3Export} from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
+import {nodePolyfills} from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
   plugins: [
     react(),
-    crx({ manifest }),
+    nodePolyfills({
+      include: ['buffer'],
+    }),
+    crx({ manifest: manifest as ManifestV3Export }),
   ],
 })
