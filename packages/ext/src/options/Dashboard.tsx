53d172005d1b48d7ad665ea289f6877e5345ae6b
Paul Burke <paul@paulburke.co>
2024-03-24 09:47:26 -0400

    feat: adds trending mints on Base

--- packages/ext/src/options/Dashboard.tsx
+++ packages/ext/src/options/Dashboard.tsx
@@ -6,12 +6,43 @@ import {ScrollArea, ScrollBar} from '@/components/ui/scroll-area'
 import {useEffect, useState} from 'react'
 import TabFrame from '@/components/TabFrame.tsx'
 import {Profile} from '@/types/Profile.ts'
+import {useQuery} from '@airstack/airstack-react'
+
+const getTrendingMints = `
+query GetTrendingMints() {
+  TrendingMints(
+    input: {
+      timeFrame: one_day,
+      audience: farcaster,
+      blockchain: base,
+      criteria: unique_wallets,
+      limit: 10
+    }
+  ) {
+    TrendingMint {
+      address
+      erc1155TokenID
+      criteriaCount
+      timeFrom
+      timeTo
+      token {
+        name
+        symbol
+        type
+      }
+    }
+  }
+}
+`
 
 const Dashboard = () => {
     const [favorites, setFavorites] = useChromeStorageSync<string[]>('bezel.favorites', [])
     const [trending, setTrending] = useState<string[]>([])
     const [forYou, setForYou] = useState<string[]>([])
+    const [mints, setMints] = useState<string[]>([])
     const [profile] = useChromeStorageLocal<Profile>('bezel.profile')
+
+    const {data, loading, error} = useQuery(getTrendingMints)
 
     useEffect(() => {
         fetch('https://graph.cast.k3l.io/frames/global/rankings?details=false&agg=sumsquare&weights=L1C10R5&offset=0&limit=10')
@@ -20,17 +51,22 @@ const Dashboard = () => {
     }, [])
 
     useEffect(() => {
-        if (!profile) return;
-        fetch(  'https://graph.cast.k3l.io/frames/personalized/rankings/fids?agg=sumsquare&weights=L1C10R5&k=2&limit=10', {
+        if (!profile) return
+        fetch('https://graph.cast.k3l.io/frames/personalized/rankings/fids?agg=sumsquare&weights=L1C10R5&k=2&limit=10', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify([profile.fid])
         })
             .then(response => response.json())
             .then(data => setForYou(data.result.map(r => r.url)))
     }, [profile])
+
+    useEffect(() => {
+        if (!data) return
+        setMints(data.TrendingMints.TrendingMint.map((mint: any) => `https://zora.co/collect/base:${mint.address}/${mint.erc1155TokenID ?? ''}`))
+    }, [data])
 
     function toggleFavorite(url: string) {
         if (url) return
@@ -94,6 +130,49 @@ const Dashboard = () => {
                     <ScrollArea className="w-full whitespace-nowrap pt-4">
                         <ul className="flex w-max gap-6">
                             {forYou.map((frame) => (
+                                <li key={frame} className="relative">
+                                    <Card onClick={() => chrome.tabs.create({url: frame})}
+                                          className="w-[300px] rounded-[0.75rem] overflow-hidden cursor-pointer">
+                                        <TabFrame url={frame}/>
+                                    </Card>
+                                    <button onClick={() => toggleFavorite(frame)}
+                                            className="absolute right-4 top-4 bg-none">
+                                        {favorites.some(f => f === frame) ?
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                 className="w-5 h-5"
+                                                 viewBox="0 0 576 512">
+                                                <path
+                                                    d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/>
+                                            </svg>
+                                            :
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                 className="w-5 h-5"
+                                                 viewBox="0 0 576 512">
+                                                <path
+                                                    d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.7 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z"/>
+                                            </svg>
+                                        }
+                                    </button>
+                                </li>
+                            ))}
+                        </ul>
+                        <div className="pt-6">
+                            <ScrollBar orientation="horizontal"/>
+                        </div>
+                    </ScrollArea>
+                </div>
+
+                <div className="flex flex-col gap-1">
+                    <h2 className="text-3xl font-semibold tracking-tight flex gap-2 items-center">
+                        <svg width="24" height="24" viewBox="0 0 111 111" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M54.921 110.034C85.359 110.034 110.034 85.402 110.034 55.017C110.034 24.6319 85.359 0 54.921 0C26.0432 0 2.35281 22.1714 0 50.3923H72.8467V59.6416H3.9565e-07C2.35281 87.8625 26.0432 110.034 54.921 110.034Z" fill="#0052FF"/>
+                        </svg>
+                        Trending Mints on Base
+                    </h2>
+                    <p className="text-base opacity-80">Get them before it's too late!</p>
+                    <ScrollArea className="w-full whitespace-nowrap pt-4">
+                        <ul className="flex w-max gap-6">
+                            {mints.map((frame) => (
                                 <li key={frame} className="relative">
                                     <Card onClick={() => chrome.tabs.create({url: frame})}
                                           className="w-[300px] rounded-[0.75rem] overflow-hidden cursor-pointer">
--- packages/ext/src/options/main.tsx
+++ packages/ext/src/options/main.tsx
@@ -4,6 +4,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Options from './Options.tsx'
 import {AuthKitProvider} from '@farcaster/auth-kit'
+import { init } from "@airstack/airstack-react";
+
+init(import.meta.env.VITE_AIRSTACK_API_KEY);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
